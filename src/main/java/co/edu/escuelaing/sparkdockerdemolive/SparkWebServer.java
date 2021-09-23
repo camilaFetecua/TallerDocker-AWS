8d92cced0fef9e6fee013ba49f9bb2fbfd2106e3
camilaFetecua <maria.fetecua@mail.escuelaing.edu.co>
2021-09-23 08:55:55 -0500

    Bosquejo

---
+++ src/main/java/co/edu/escuelaing/sparkdockerdemolive/SparkWebServer.java
@@ -0,0 +1,25 @@
+package co.edu.escuelaing.sparkdockerdemolive;
+
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class SparkWebServer
+{
+    public static void main(String... args){
+        port(getPort());
+        get("hello", (req,res) -> "Hello Docker!");
+    }
+
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+    }
+}
